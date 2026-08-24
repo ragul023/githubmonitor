0ebc0016756edefe68b42cda5e9c76963b9b7d3b
ragul023 <ragul4172@gmail.com>
2026-08-24 19:14:44 +0530

    Testing Event Handlers

---
+++ Resues.r
@@ -0,0 +1,8 @@
+# Basic math and text assignment
+user_name <- "Alex"
+hourly_rate <- 45.50
+hours_worked <- 40
+
+# Calculate total pay
+total_pay <- hourly_rate * hours_worked
+print(total_pay) # Outputs: 1820
